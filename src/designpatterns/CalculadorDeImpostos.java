0dcf70036ad656cb037a6bfe2e6472295b5929f2
Rodrigo Bresan <rcbresan@gmail.com>
2016-05-17 10:15:09 -0300

    One more tax added

--- src/designpatterns/CalculadorDeImpostos.java
+++ src/designpatterns/CalculadorDeImpostos.java
@@ -2,9 +2,15 @@ package designpatterns;
 
 public class CalculadorDeImpostos {
 
-	public void realizaCalculo(Orcamento orcamento) {
-		double icms = orcamento.getValor() * 0.1;
-		System.out.println(icms);
+	public void realizaCalculo(Orcamento orcamento, String imposto) {
+		
+		if (imposto.equals("ICMS")) {
+			double icms = orcamento.getValor() * 0.1;
+			System.out.println(icms);
+		} else if (imposto.equals("ISS")) {
+			double iss = orcamento.getValor() * 0.6;
+			System.out.println(iss);
+		}
 	}
 	
 }
